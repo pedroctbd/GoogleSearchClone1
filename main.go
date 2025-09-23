09886f178ffa8c75d1bb787fa5140a591cf74160
DESKTOP-3233IF1\Pedro <pedroctbd@gmail.com>
2025-09-22 23:25:02 -0300

    Creation of queue and message publishing

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"time"
@@ -9,6 +11,7 @@ import (
 	"github.com/elastic/go-elasticsearch/v9"
 	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
+	"github.com/streadway/amqp"
 	"googlesearchclone1.com/utils"
 )
 
@@ -17,6 +20,12 @@ type Document struct {
 	Count     int       `json:"count"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type QueueTask struct {
+	Id        string
+	Prefix    string
+	updatedAt time.Time
 }
 
 func main() {
@@ -44,9 +53,58 @@ func main() {
 		panic(err)
 	}
 
+	//RabbitMQ config
+	conn, err := amqp.Dial(os.Getenv("http://localhost:5672"))
+	if err != nil {
+		log.Fatalf("failed to connect to RabbitMQ: %v", err)
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("failed to open a channel: %v", err)
+	}
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"jobs",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Fatalf("failed to declare a queue: %v", err)
+	}
+
 	r.Get("/complete/search", func(w http.ResponseWriter, r *http.Request) {
 
 		searchPrefix := r.URL.Query().Get("q")
+
+		//update count
+		newM := QueueTask{
+			Id:        "1",
+			updatedAt: time.Now(),
+			Prefix:    searchPrefix,
+		}
+		body, err := json.Marshal(newM)
+		if err != nil {
+			log.Fatalf("failed to marshal message: %v", err)
+		}
+		err = ch.Publish(
+			"",
+			q.Name,
+			false,
+			false,
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        body,
+			},
+		)
+		if err != nil {
+			log.Fatalf("failed to publish a message: %v", err)
+		}
 
 		utils.Encode(w, r, http.StatusAccepted, fmt.Sprintf("current prefix = %s", searchPrefix))
 		return

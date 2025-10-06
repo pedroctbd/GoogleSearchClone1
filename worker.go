3efc31f899697da709ec807082ea7bc98af81fb0
Pedro Badaro <pedroctbd@gmail.com>
2025-10-06 15:49:49 -0300

    Removed auto ack in queue

--- worker.go
+++ worker.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"strings"
@@ -14,7 +15,7 @@ func StartWorker(es *elasticsearch.Client, ch *amqp.Channel) {
 	msgs, err := ch.Consume(
 		"search-terms",
 		"",
-		true,
+		false,
 		false,
 		false,
 		false,
@@ -24,38 +25,68 @@ func StartWorker(es *elasticsearch.Client, ch *amqp.Channel) {
 		log.Fatalf("failed to register a consumer: %v", err)
 	}
 
-	log.Printf(" [*] Worker is waiting for messages.")
+	log.Printf("Worker is waiting for messages.")
 
-	go func() {
-		for d := range msgs {
-			searchTerm := string(d.Body)
-			log.Printf("Received a message: %s", searchTerm)
-			updateSuggestion(es, searchTerm)
+	for d := range msgs {
+		searchTerm := string(d.Body)
+		log.Printf("Received a message: %s", searchTerm)
+		err := updateSuggestion(es, searchTerm)
+		if err != nil {
+			log.Printf("error processing message %s", err)
+			d.Nack(false, true)
+		} else {
+			d.Ack(false)
 		}
-	}()
+	}
+
 }
 
-func updateSuggestion(es *elasticsearch.Client, searchTerm string) {
+func updateSuggestion(es *elasticsearch.Client, searchTerm string) error {
+
 	docID := searchTerm
 
-	script := `{
-		"script": {
-			"source": "ctx._source.suggest.weight += 1",
-		},
-		"upsert": {
-			"suggest": {
-				"input": ["%s"],
-				"weight": 1
-			}
-		}
-	}`
+	escapedTerm, err := json.Marshal(searchTerm)
+	if err != nil {
+		return fmt.Errorf("failed to marshal search term: %w", err)
+	}
 
-	scriptWithTerm := fmt.Sprintf(script, searchTerm)
-	_, err := es.Update("search-terms", docID, strings.NewReader(scriptWithTerm), es.Update.WithContext(context.Background()))
+	scriptTemplate := `
+	{
+        "scripted_upsert": true,
+        "script": {
+            "source": "ctx._source.search_count += 1; ctx._source.suggest.weight = ctx._source.search_count;",
+            "lang": "painless"
+        },
+        "upsert": {
+            "suggest": {
+                "input": [%s],
+                "weight": 1
+            },
+            "search_count": 1
+        }
+    }`
 
+	requestBody := fmt.Sprintf(scriptTemplate, escapedTerm)
+
+	res, err := es.Update(
+		"search-terms",
+		docID,
+		strings.NewReader(requestBody),
+		es.Update.WithContext(context.Background()),
+		es.Update.WithRefresh("true"),
+	)
 	if err != nil {
 		log.Printf("Error updating document %s: %s", docID, err)
-	} else {
-		log.Printf("Successfully updated document: %s", docID)
+		return err
 	}
+	defer res.Body.Close()
+
+	if res.IsError() {
+		err := fmt.Errorf("error response from Elasticsearch for doc %s: %s", docID, res.String())
+		log.Print(err)
+		return err
+	}
+
+	log.Printf("Successfully updated document: %s", docID)
+	return nil
 }
